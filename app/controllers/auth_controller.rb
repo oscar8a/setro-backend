3e8fc98edcada81ad77e8e11a073d730419d9269
github username <oscar8atech@gmail.com>
2019-10-31 23:54:26 -0400

    fetch of products authorized

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -4,9 +4,8 @@ class AuthController < ApplicationController
   def create # POST /login
     @user = User.find_by(email: login_params[:email])
     # @user.authenticate('password')
-    byebug
     if @user && @user.authenticate(login_params[:password])
-      @token = encode_token({ password: @user.password })
+      @token = encode_token({ user_id: @user.id })
       render json: { user: @user, jwt: @token }, status: :accepted
     else
       render json: { errors: 'Invalid username or password' }, status: :unauthorized
