145214345f78d8f3a04f0f7b32eb6178fdab4a94
github username <oscar8atech@gmail.com>
2019-11-07 16:46:48 -0500

    worked on order placement

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,5 @@
 class OrdersController < ApplicationController
-
+  skip_before_action :authorized
   def index
     #if logged_in_user.admin?
       orders = Order.all.order(created_at: :desc)
@@ -8,6 +8,17 @@ class OrdersController < ApplicationController
     #   render json: { errors: 'product.errors.full_messages' }
     # end
   end 
+
+  def create 
+    order = Order.create(order_params)
+
+    render json: order
+  end
+
+  private
+  def order_params
+    params.require(:order).permit(:user_id, :status, :date)
+  end
   
 
 end
